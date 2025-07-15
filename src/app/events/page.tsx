81a9bbeb4618b64b158024e8344e7156688eab7d
danieleduar0911 <danieleduar0911@gmail.com>
2025-07-15 03:36:28 -0500

    default export to events page

--- src/app/events/page.tsx
+++ src/app/events/page.tsx
@@ -1,7 +1,12 @@
+// src/app/events/page.tsx
+
 export default function EventsPage() {
   return (
-    <div>
-      <h1>Eventos</h1>
+    <div className="p-8">
+      <h1 className="text-3xl font-bold text-gray-800">Eventos</h1>
+      <p className="mt-2 text-gray-600">
+        Aquí se mostrarán los eventos del distrito.
+      </p>
     </div>
   );
 }
--- src/app/noticias/page.tsx
+++ src/app/noticias/page.tsx
@@ -1,7 +1,10 @@
-export default function EventsPage() {
+// src/app/events/page.tsx
+
+export default function NoticiasPage() {
   return (
-    <div>
-      <h1>noticias</h1>
+    <div className="p-8">
+      <h1 className="text-3xl font-bold text-gray-800">Noticias</h1>
+      <p className="mt-2 text-gray-600">Aquí se mostrarán las noticas</p>
     </div>
   );
 }
